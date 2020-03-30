b3480fbe6589cde928bb1031b4d18f95a71e40f8
Quantum <liuk_me@outlook.com>
2020-03-31 03:40:25 +0800

    footer.php链接在新页面打开

--- cwlog/footer.php
+++ cwlog/footer.php
@@ -1,6 +1,6 @@
 </div>
 <div id="footer">
-  <p><i class="fa fa-copyright"></i> <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>" target="_blank"><?php $this->options->title(); ?></a> Powered by <a href="http://typecho.org">Typecho</a> & <a href="https://cwlog.net" target="_blank">cwlog.net</a></p>
+  <p><i class="fa fa-copyright"></i> <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a> Powered by <a href="http://typecho.org" target="_blank">Typecho</a> & <a href="https://cwlog.net" target="_blank">cwlog.net</a></p>
   <?php if($this->options->icpNum){ ?>
     <p><a href="http://www.beian.miit.gov.cn/" target="_blank"><?php $this->options->icpNum(); ?></a></p>
   <?php }?>
